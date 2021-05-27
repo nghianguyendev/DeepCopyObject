0f426f3887c436086a20bd3d93afb88f9ae4ac14
nghianguyendev <nghianguyentien@yahoo.com>
2021-05-27 08:29:22 +0700

    Deep copy object using xml serialization

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+using Coding.Exercise;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Line line = new Line
+            {
+                End = new Point { X = 1, Y = 2 },
+                Start = new Point { X = 3, Y = 4 }
+            };
+            Line line2 = line.DeepCopyXml();
+            line2.Start = new Point { X = 4, Y = 5 };
+            Console.WriteLine("Hello");
+            WriteLine(line.Start.X);
+            WriteLine(line2.Start.X);
+
+        }
+    }
+}
